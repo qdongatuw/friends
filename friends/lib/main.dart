89ba27d71f8d7331b7c51f22f8d64d6c413796ea
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-08-04 19:00:42 -0500

    1

--- friends/lib/main.dart
+++ friends/lib/main.dart
@@ -21,24 +21,111 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
   FloatingActionButtonLocation _fabLocation =
       FloatingActionButtonLocation.endDocked;
 
-  int season = 1;
-  int episode = 1;
-  List<List> favorites = [];
+  bool darkTheme = false;
+  int season = 0;
+  int episode = 0;
+List<List<List<int>>> favorites = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (int i=0; i<cc.length; i++){
+      favorites.add([]);
+      for (int j=0; j<cc[i].length; j++){
+        favorites[i].add([]);
+      }
+    }
+  }
+  
 
-  void addToFavorites(List item) {
+  void addToFavorites(int indexSeason, int indexEpisode, int indexLine) {
     setState(() {
-      favorites.add(item);
+      favorites[indexSeason][indexEpisode].add(indexLine);
     });
   }
+
+  void removeFavorites(int indexSeason, int indexEpisode, int indexLine) {
+    setState(() {
+      favorites[indexSeason][indexEpisode].removeAt(indexLine);
+    });
+  }
+
+  // void showAll(BuildContext context){
+  //   showModalBottomSheet<void>(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Container(
+  //         child: ListView.builder(
+  //             scrollDirection: Axis.horizontal,
+  //             itemCount: cc.length,
+  //             padding: EdgeInsets.all(10),
+              
+  //             itemBuilder: (context, index){
+  //               return Column(
+  //                 children: [
+  //                   Image.asset('lib/assets/1.jpg'),
+  //                   Center(child: Text('test')),
+  //                   // ListView.builder( 
+  //                   //     itemCount:cc[index].length, 
+  //                   //     itemBuilder: (context1, indexEpisode){
+  //                   //     return Text('Episode ${indexEpisode+1}');
+  //                   //   }),
+                    
+                    
+  //                 ],
+  //               );
+  //             })
+  //       );
+  //     });
+  // }
+
+  void showAll(BuildContext context) {
+  showModalBottomSheet<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        height: 600, // 设置底部弹出面板的高度
+        child:
+        Scrollbar(
+          thumbVisibility: true,
+          child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: cc.length,
+          padding: EdgeInsets.all(10),
+          itemBuilder: (context, index) {
+            return Container(
+              width: 150, // 设置列表项的宽度
+              margin: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Image.asset('lib/assets/1.jpg'),
+                  SizedBox(height: 8),
+                  Text('test')
+                ],
+              ),
+            );
+          },
+        ),)
+         
+      );
+    },
+  );
+}
+
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(),
+        // textTheme: GoogleFonts.notoSerifTextTheme(), 
+      ),
       home: Scaffold(
-        
+        appBar: AppBar(title: Text('Season ${season+1} - Episode ${episode+1}', style: GoogleFonts.lobster(),),),
         body: ListView.builder(
           
-          itemCount: s1[1]?.length,
+          itemCount: cc[season][episode]?.length,
           itemBuilder: (context, index){
             return Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -46,33 +133,58 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
                     color: Colors.amberAccent,
                     // titleTextStyle: TextStyle(fontSize: 14),
                     title: SelectableText(
-            s1[1]?[index][1],
-            style: TextStyle(fontSize: 20),
+            cc[season][episode+1]?[index][1],
             onTap: () {
             },
-            
           ),
                     
-                    subtitle: Text(s1[1]?[index][0]),
+                    subtitle: Text(cc[season][episode+1]?[index][0], style: GoogleFonts.lobster(),),
                     index: index,
                     seasonIndex: season,
                     episodeIndex: episode,
                     addToFavorites: addToFavorites,
-                    isFavorite: favorites.contains([season, episode, index]),
+                    removeFavorites: removeFavorites,
+                    isFavorite: favorites[season][episode].contains(index),
           )
 
                 );
         }),
         floatingActionButton: FloatingActionButton(
-                onPressed: () {},
-                tooltip: 'Create',
-                child: const Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    if(season==cc.length-1&&episode==cc[season].length-1){
+                      return;
+                    }
+                    if(episode < cc[season].length-1)
+                      {episode++;}
+                    else{
+                      episode = 0;
+                      season++;
+                    }
+                  });
+                },
+                tooltip: 'Nest episode',
+                child: const Icon(Icons.forward),
               ),
 
         floatingActionButtonLocation: _fabLocation,
         bottomNavigationBar: _DemoBottomAppBar(
           fabLocation: _fabLocation,
           shape: const CircularNotchedRectangle(),
+          lastChapter: (){
+            setState(() {
+              if (episode == 0&&season==0){
+                return;
+              }
+              if (episode==0){
+                season--;
+                episode = cc[season].length-1;
+                return;
+              }
+              episode--;
+            });
+          },
+          showChapters: showAll,
         ),
       ),
     );
@@ -83,7 +195,8 @@ class ListTileWithFavorite extends StatefulWidget {
   final Widget title;
   final Widget subtitle;
   final Color color;
-  final Function(List) addToFavorites;
+  final Function(int, int, int) addToFavorites;
+  final Function(int, int, int) removeFavorites;
   final bool isFavorite;
   final int seasonIndex;
   final int episodeIndex;
@@ -94,6 +207,7 @@ class ListTileWithFavorite extends StatefulWidget {
     required this.subtitle,
     required this.color,
     required this.addToFavorites,
+    required this.removeFavorites,
     required this.isFavorite,
     required this.seasonIndex,
     required this.episodeIndex,
@@ -119,9 +233,9 @@ class _ListTileWithFavoriteState extends State<ListTileWithFavorite> {
     });
 
     if (_isFavorite) {
-      widget.addToFavorites([widget.seasonIndex, widget.episodeIndex, widget.index]);
+      widget.addToFavorites(widget.seasonIndex, widget.episodeIndex, widget.index);
     } else {
-      // Remove from favorites
+      widget.removeFavorites(widget.seasonIndex, widget.episodeIndex, widget.index);
     }
   }
 
@@ -129,14 +243,14 @@ class _ListTileWithFavoriteState extends State<ListTileWithFavorite> {
   Widget build(BuildContext context) {
     return Card(
       elevation: 20,
-      shadowColor: Colors.grey,
-      color: widget.color,
+      // shadowColor: Colors.grey,
+      // color: widget.color,
       child: ListTile(
       title: widget.title,
       subtitle: widget.subtitle,
       //tileColor: widget.color,
       trailing: IconButton(
-        icon: _isFavorite ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+        icon: _isFavorite ? const Icon(Icons.favorite, color: Colors.red,) : const Icon(Icons.favorite_border),
         onPressed: _toggleFavorite,
       ),
     )); 
@@ -147,10 +261,14 @@ class _DemoBottomAppBar extends StatelessWidget {
   const _DemoBottomAppBar({
     this.fabLocation = FloatingActionButtonLocation.endDocked,
     this.shape = const CircularNotchedRectangle(),
+    required this.lastChapter,
+    required this.showChapters,
   });
 
   final FloatingActionButtonLocation fabLocation;
   final NotchedShape? shape;
+  final Function lastChapter;
+  final Function showChapters;
 
   static final List<FloatingActionButtonLocation> centerLocations =
       <FloatingActionButtonLocation>[
@@ -162,21 +280,22 @@ class _DemoBottomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomAppBar(
       shape: shape,
-      color: Colors.blue,
-      child: IconTheme(
-        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-        child: Row(
+      child: Row(
           children: <Widget>[
             IconButton(
-              tooltip: 'Open navigation menu',
+              tooltip: 'Select Season and episode',
               icon: const Icon(Icons.menu),
-              onPressed: () {},
+              onPressed: (){
+                showChapters(context);
+              },
             ),
             if (centerLocations.contains(fabLocation)) const Spacer(),
             IconButton(
-              tooltip: 'Search',
-              icon: const Icon(Icons.search),
-              onPressed: () {},
+              tooltip: 'Previous episode',
+              icon: const Icon(Icons.arrow_left),
+              onPressed: () {
+                lastChapter();
+              },
             ),
             IconButton(
               tooltip: 'Favorite',
@@ -185,7 +304,6 @@ class _DemoBottomAppBar extends StatelessWidget {
             ),
           ],
         ),
-      ),
     );
   }
 }
